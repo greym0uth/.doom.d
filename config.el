9ab4d61d3b8b0f80769c8b866e9f970126066ac8
Jaden Giordano <thejadenjack@gmail.com>
2021-05-01 16:52:55 -0700

    update

---
+++ config.el
@@ -0,0 +1,85 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Jaden Giordano"
+      user-mail-address "thejadenjack@gmail.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Fira Mono for Powerline" :size 14)
+      doom-variable-pitch-font (font-spec :family "open sans"))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+(add-to-list `initial-frame-alist `(fullscreen . maximized))
+
+;; Rust configs
+;; Use rustic-mode for RON files.
+(add-to-list `auto-mode-alist '("\\.ron\\'" . rustic-mode))
+
+(after! projectile
+  (setq projectile-project-search-path `("~/Development" "~/Development/rust"))
+  (projectile-register-project-type 'godot '("project.godot")))
+
+(after! company
+  (setq company-idle-delay 0.5))
+
+(server-start)
+
+;; GDScript
+;; Temporary workaround issues with the language server
+(defun franco/godot-gdscript--lsp-ignore-error (original-function &rest args)
+  "Ignore the error message resulting from Godot not replying to the `JSONRPC' request."
+  (if (string-equal major-mode "gdscript-mode")
+      (let ((json-data (nth 0 args)))
+        (if (and (string= (gethash "jsonrpc" json-data "") "2.0")
+                 (not (gethash "id" json-data nil))
+                 (not (gethash "method" json-data nil)))
+            nil
+          (apply original-function args)))
+    (apply original-function args)))
+
+(after! lsp
+  (advice-add #'lsp--get-message-type :around #'franco/godot-gdscript--lsp-ignore-error))
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
